94afeef4823b77552819478b95e0bef5627ed40a
divaniuss <dimasikemelka@gmail.com>
2026-07-23 10:21:14 +0300

    created FavoriteModule and integrated it into TabBar

---
+++ NewsAppp/NewsAppp/Tabs/FavoritesModule/protocol/FavoritesProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesPresenterProtocol.swift
+//  NewsAppp
+//
+//  Created by ios developer on 22.07.2026.
+//
+
+import Foundation
+
+protocol FavoritesViewProtocol{
+    func reloadData()
+    func toggleEmptyState(isHidden: Bool)
+}
+
+protocol FavoritesPresenterProtocol{
+    
+}
